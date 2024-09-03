64a1655aa1b9e83a994338089a3caf7f80c454ea
pedro martingaste <pitermartingaste@gmail.com>
2024-09-03 00:48:08 -0300

    background image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import React from "react";
+import Image from "next/image";
 import "./globals.css";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
@@ -15,6 +17,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Image
+                src="/jugocup.png"
+                alt='Placeholder'
+                width={0}
+                height={0}
+                sizes="100vw"
+                placeholder="empty"
+                loading="lazy"
+                style={{height: "25r100vhem", width: "auto", position: 'absolute', top: '10rem', zIndex: '-100', margin: "auto"}}
+              />
       <body className={inter.className}>
         <Nav/>
         {children}
